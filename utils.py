41b9dd61f35a43f35b84bb8183718ce7acb02e28
Alex43063 <49129505+Alex43063@users.noreply.github.com>
2020-03-16 17:10:19 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 def convert_time_to_string(dt):
-    return f"{dt.hour} {dt.minute:02}"
+    return f"{dt.hour}:{dt.minute:02}"
 def time_has_changed(prev_time):
     return convert_time_to_string(datetime.now()) != prev_time
 
